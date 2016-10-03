088737c5ab45e3f24fdb4233f9064f954f906d3e
Kevin Aud <kevinaud@gmail.com>
2016-10-03 15:31:56 -0500

    added form validation in angular

--- src/main/resources/public/sign-up/sign-up.controller.js
+++ src/main/resources/public/sign-up/sign-up.controller.js
@@ -2,41 +2,18 @@
   'use strict';
 
     angular.module('app')
-      .controller('signUpController', ['$scope', '$http', function($scope, $http) {
+      .controller('signUpController', ['$scope', '$user', function($scope, $user) {
       
-      var user = {
+      $scope.user = {
         email: '',
         name: '',
         password: '',
-        confirmPassword: ''
+        password_verify: ''
       };
 
       $scope.submitForm = function() {
-        
-        let payload = JSON.stringify({
-          email: this.user.email,
-          name: this.user.name,
-          password: this.user.password
-        });
-
-        var req = {
-          method: 'POST',
-          url: 'http://localhost:8080/sign-up',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: payload 
-        }
-
-        $http(req).then(function(response){
-          console.log('SUCCESS', response);
-
-          let data = JSON.parse(response.data.message);
-          console.log(data.token);
-
-        }, function(response){
-          console.log('ERROR', response); ActiveXObject             
-        });
+    
+        $user.signUp(this.user);
 
       };
 
