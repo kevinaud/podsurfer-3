e99761ddca100b0affcccb7a86e66560cd5e305b
Jack <Jack_Swenson@Baylor.edu>
2016-10-03 10:38:39 -0500

    setup post request on login page

--- src/main/resources/public/login/login.controller.js
+++ src/main/resources/public/login/login.controller.js
@@ -1,37 +1,40 @@
 (function() {
   'use strict';
 
-    angular.module('app')
-      .controller('loginController', ['$scope','$http'], function($scope, $http) {
-        var user = {
-          username: '',
-          password: ''
-        };
+  angular.module('app')
+    .controller('loginController', ['$scope','$http', function($scope, $http)
+    {
+      var user = {
+        username: '',
+        password: ''
+      };
 
-        $scope.submitForm = function() {
+      if(user.email.length() > 0 && user.name.length() > 0
+        && user.password.length() > 0
+        && user.confirmPassword.length() === user.password.length())
+      {
+        $scope.submitForm = function()
+        {
+          var payload = JSON.stringify(user);
+          var req = {
+            method: 'POST',
+            url: 'http://localhost:8080/login',
+            headers: { 'Content-Type': 'application/json' },
+            data: payload
+          }
 
-            let payload = JSON.stringify(user);
-            var req = {
-                method: 'POST',
-                url: 'http://localhost:8080/login',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                data: payload
+          $http(req).then(
+            function(response) {
+              console.log('SUCCESS', response);
+
+              var data = JSON.parse(response.data.message);
+              console.log(data.token);
+            },
+            function(response) {
+              console.log('ERROR', response);
             }
-
-            $http(req).then(
-                function(response) {
-                    console.log('SUCCESS', response);
-
-                    let data = JSON.parse(response.data.message);
-                    console.log(data.token);
-                },
-                function(response) {
-                    console.log('ERROR', response);
-                }
-            )
+          );
         }
-
-      })
-});
+      }
+    }]);
+})();
