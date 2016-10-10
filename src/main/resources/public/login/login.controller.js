ea6c16e2ad657c1596ac9b50fa2d2c93746220cf
Jack <Jack_Swenson@Baylor.edu>
2016-10-10 12:59:35 -0500

    added response error to sign up

--- src/main/resources/public/login/login.controller.js
+++ src/main/resources/public/login/login.controller.js
@@ -1,21 +1,20 @@
 (function() {
   'use strict';
 
-    angular.module('app')
-      .controller('loginController', ['$scope','$user', function($scope, $user) {
+  angular.module('app')
+    .controller('loginController', ['$scope','$user', function($scope, $user) {
 
+    $scope.user = {
+      email: '',
+      password: ''
+    };
 
-      $scope.user = {
-        email: '',
-        password: ''
-      };
+    $scope.responseError = false;
 
-      $scope.responseError = false;
-
-      $scope.submitForm = function() {
-        $scope.responseError = true;
-        $user.login($scope.user);
-      }
-    }]);
+    $scope.submitForm = function() {
+      $scope.responseError = true;
+      $user.login($scope.user);
+    }
+  }]);
 
 })();
