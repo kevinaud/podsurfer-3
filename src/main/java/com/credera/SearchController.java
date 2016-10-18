225e7555d60efd541a99e74516cdd75228e86fcd
Kevin Aud <kevinaud@gmail.com>
2016-10-18 16:31:40 -0500

    added general search endpoint and endpoints for seaching only podcast or only episodes (#14)

--- src/main/java/com/credera/SearchController.java
+++ src/main/java/com/credera/SearchController.java
@@ -2,7 +2,9 @@ package com.credera;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.elasticsearch.Elasticsearch;
@@ -19,6 +21,24 @@ public class SearchController {
     @RequestMapping("/search/health")
     public String getClusterHealth(){
         return es.getHealth();
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/search", method = RequestMethod.POST)
+    public String searchAll(@RequestBody SearchQuery searchQuery){
+        return es.searchAll(searchQuery);
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/search/podcast", method = RequestMethod.POST)
+    public String searchPodcasts(@RequestBody SearchQuery searchQuery){
+        return es.searchPodcast(searchQuery);
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/search/episode", method = RequestMethod.POST)
+    public String searchEpisodes(@RequestBody SearchQuery searchQuery){
+        return es.searchEpisode(searchQuery);
     }
 
 }
