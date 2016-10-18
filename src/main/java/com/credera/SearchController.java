9aafd9850b1d959b261ece7693d7b9ec90121ad4
Kevin Aud <kevinaud@gmail.com>
2016-10-18 16:52:49 -0500

    Search podcasts (#16)
    * added general search endpoint and endpoints for seaching only podcast or only episodes
    
    * added get all endpoints

--- src/main/java/com/credera/SearchController.java
+++ src/main/java/com/credera/SearchController.java
@@ -24,20 +24,38 @@ public class SearchController {
     }
 
     @ResponseBody
+    @RequestMapping(value="/search", method = RequestMethod.GET)
+    public String searchAll(){
+        return es.searchAll();
+    }
+
+    @ResponseBody
     @RequestMapping(value="/search", method = RequestMethod.POST)
     public String searchAll(@RequestBody SearchQuery searchQuery){
         return es.searchAll(searchQuery);
     }
 
     @ResponseBody
+    @RequestMapping(value="/search/podcast", method = RequestMethod.GET)
+    public String searchPodcast(){
+        return es.searchPodcast();
+    }
+
+    @ResponseBody
     @RequestMapping(value="/search/podcast", method = RequestMethod.POST)
-    public String searchPodcasts(@RequestBody SearchQuery searchQuery){
+    public String searchPodcast(@RequestBody SearchQuery searchQuery){
         return es.searchPodcast(searchQuery);
     }
 
     @ResponseBody
+    @RequestMapping(value="/search/episode", method = RequestMethod.GET)
+    public String searchEpisode(){
+        return es.searchPodcast();
+    }
+
+    @ResponseBody
     @RequestMapping(value="/search/episode", method = RequestMethod.POST)
-    public String searchEpisodes(@RequestBody SearchQuery searchQuery){
+    public String searchEpisode(@RequestBody SearchQuery searchQuery){
         return es.searchEpisode(searchQuery);
     }
 
