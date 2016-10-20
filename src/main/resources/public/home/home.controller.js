08973637b121ec4ed4511db720c6a957b9261f28
Kevin Aud <kevinaud@gmail.com>
2016-10-20 12:05:36 -0500

    removed credera example code (#18)

--- src/main/resources/public/home/home.controller.js
+++ src/main/resources/public/home/home.controller.js
@@ -4,30 +4,11 @@
     angular.module('app')
       .controller('homeController', homeController);
 
-    homeController.$inject = ['homeEndpoints'];
-    function homeController(homeEndpoints) {
+    function homeController() {
       var vm = this;
 
-      vm.welcome = 'Welcome to PodSurfer';
       vm.signupPrompt = 'New to PodSurfer? Create an account!'
-      vm.outOfZen = false;
-      vm.getZen = getZen;
 
-      function getZen() {
-        var success = function(response) {
-          vm.outOfZen = false;
-          console.log('Got Zen');
-          vm.zen = response.data;
-        };
-
-        var error = function(response) {
-          vm.outOfZen = true;
-          console.error('Failed to get Zen');
-          vm.zen = response.data.message;
-        };
-
-        return homeEndpoints.getZen().then(success, error);
-      }
     }
 
 })();
