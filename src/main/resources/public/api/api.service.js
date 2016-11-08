dc3540476a994e8edf605e49f0c4d32e6a556b17
Kevin Aud <kevinaud@gmail.com>
2016-11-08 15:54:38 -0600

    Unit tests (#39)
    * Modified Podcast
    
    * added unit tests
    
    * added comment
    
    * configured automatic replacement of api endpoint
    
    * added automatic api endpoint replacement

--- src/main/resources/public/api/api.service.js
+++ src/main/resources/public/api/api.service.js
@@ -14,7 +14,12 @@
     return exports;
 
     function getUrl() {
+<<<<<<< HEAD
+      let apiUrl = "http://localhost:8080";
+      return apiUrl;
+=======
       return "http://localhost:8080";
+>>>>>>> f045051ff15f01863d94bf04e64ce2de263ddc47
     }
   }
 })();
