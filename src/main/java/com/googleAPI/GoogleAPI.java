3a55335a5acc47cb46051d78bb128499421711df
Jack <Jack_Swenson@Baylor.edu>
2016-11-28 20:07:10 -0600

    added shell for googleAPI

---
+++ src/main/java/com/googleAPI/GoogleAPI.java
@@ -0,0 +1,35 @@
+package com.googleAPI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.springframework.social.connect.*;
+
+import com.credera.Response;
+import com.credera.User;
+
+@Service
+public class GoogleAPI {
+
+  private String apiUrl = "fb-dev-url";
+  private User user;
+
+  public GoogleAPI(){
+  }
+
+  public boolean authorize(String token){
+    return false;
+  }
+
+  public boolean isAuthorized() {
+    return false;
+  }
+
+  public User getUserInfo(){
+    return null;
+  }
+}
