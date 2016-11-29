5ab2cc59b9ffa8c7b3e0e4fe89dc409317be2a96
Kevin Aud <kevinaud@gmail.com>
2016-11-29 17:36:58 -0600

    Recommendations (#45)
    * added unit tests
    
    * added comment
    
    * configured automatic replacement of api endpoint
    
    * added automatic api endpoint replacement
    
    * Fix build (#41)
    
    * added unit tests
    
    * Dev (#35)
    
    * Modified Podcast
    
    * added unit tests (#34)
    
    * added comment
    
    * Unit tests (#36)
    
    * Modified Podcast
    
    * added unit tests
    
    * added comment
    
    * Unit tests (#37)
    
    * Modified Podcast
    
    * added unit tests
    
    * added comment
    
    * configured automatic replacement of api endpoint
    
    * Unit tests (#38)
    
    * Modified Podcast
    
    * added unit tests
    
    * added comment
    
    * configured automatic replacement of api endpoint
    
    * added automatic api endpoint replacement
    
    * Unit tests (#39)
    
    * Modified Podcast
    
    * added unit tests
    
    * added comment
    
    * configured automatic replacement of api endpoint
    
    * added automatic api endpoint replacement
    
    * saving
    
    * saving
    
    * implemented recommendations

--- src/main/java/com/podsurferAPI/PodsurferAPI.java
+++ src/main/java/com/podsurferAPI/PodsurferAPI.java
@@ -1,5 +1,6 @@
 package com.podsurferAPI;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
@@ -7,6 +8,8 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import com.credera.*;
+
+import java.io.IOException;
 
 @Service
 public class PodsurferAPI {
@@ -26,8 +29,20 @@ public class PodsurferAPI {
 	}
 
 	public Response getUserInfo(String token) {
-        return apiGetRequest("/api/user/me", "Authorization", token);
-    }
+		return apiGetRequest("/api/user/me", "Authorization", token);
+	}
+
+	public String getUserEmail(String token) {
+		com.fasterxml.jackson.databind.JsonNode response = apiGetRequestJson("/api/user/me", "Authorization", token);
+
+		if (response != null && response.has("email")) {
+			return response.get("email").asText();
+		} else {
+			return null;
+		}
+
+	}
+
 
 	public Response apiPostRequest(String endpoint, Object payload) {
 		
@@ -70,5 +85,38 @@ public class PodsurferAPI {
     }
     return response;
   }
+
+	public com.fasterxml.jackson.databind.JsonNode apiGetRequestJson(String endpoint, String headerKey, String headerValue) {
+
+		String url = apiUrl + endpoint;
+		com.fasterxml.jackson.databind.JsonNode responseObj;
+
+		try {
+
+			HttpResponse<JsonNode> tokenResponse = Unirest.get(url)
+					.header(headerKey, headerValue)
+					.asJson();
+
+			ObjectMapper mapper = new ObjectMapper();
+
+			try {
+
+				responseObj = mapper.readTree(tokenResponse.getBody().toString());
+
+			} catch(IOException error) {
+				System.out.println(error.getMessage());
+				System.out.println(error.getStackTrace());
+				return null;
+			}
+
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+		return responseObj;
+
+	}
 }
 
